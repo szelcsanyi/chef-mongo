4475c94bda59ed04562f05c1556c96400308b524
mohitsethi <mohit@sethis.in>
2015-09-17 11:52:21 +0530

    Bumped v1.0.5

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ license          'MIT'
 description      'Installs/Configures mongodb, multi instance support'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             'L7-mongo'
-version          '1.0.10'
+version          '1.0.5'
 source_url       'https://github.com/szelcsanyi/chef-mongo'
 issues_url       'https://github.com/szelcsanyi/chef-mongo/issues'
 
 supports 'ubuntu', '>= 12.04'
+supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 7.0'
