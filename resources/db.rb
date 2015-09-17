c6f6a234db60cb4cbeeae951c63f397e5a2f05d7
mohitsethi <mohit@sethis.in>
2015-09-17 11:51:53 +0530

    using mongo v3.0.6

--- resources/db.rb
+++ resources/db.rb
@@ -7,9 +7,8 @@
 actions :create, :remove
 
 attribute :name, kind_of: String, name_attribute: true
-attribute :cookbook, kind_of: String, default: 'L7-mongo'
-
-attribute :url, kind_of: String, default: 'https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.1.tgz'
+attribute :cookbook, kind_of: String, default: 'mongo'
+attribute :url, kind_of: String, default: 'https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.6.tgz'
 attribute :home, kind_of: String, default: '/opt'
 attribute :bind_ip, kind_of: [String, Array], default: '127.0.0.1'
 attribute :port, kind_of: [Integer, String], default: '27017'
@@ -18,9 +17,6 @@ attribute :replSet, kind_of: [NilClass, String], default: nil
 attribute :notablescan, kind_of: [FalseClass, TrueClass], default: true
 attribute :smallfiles, kind_of: [FalseClass, TrueClass], default: true
 attribute :journal, kind_of: [FalseClass, TrueClass], default: true
-attribute :rest, kind_of: [FalseClass, TrueClass], default: true
-attribute :httpinterface, kind_of: [FalseClass, TrueClass], default: true
-attribute :auth, kind_of: [FalseClass, TrueClass], default: false
 
 def initialize(*args)
   super
